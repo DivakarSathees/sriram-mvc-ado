ce5a0b44b7126a2a88b1410246a8b3199d553db6
DivakarSathees <divakarsathees7@gmail.com>
2023-09-04 08:37:42 +0000

    sec setup

--- mvc1/dotnetapp/Controllers/CarserviceController.cs
+++ mvc1/dotnetapp/Controllers/CarserviceController.cs
@@ -11,46 +11,74 @@ using Microsoft.Data.SqlClient;
 
 public class CarserviceController : Controller
 {
-    private string connectionString = "User ID=sa;password=examlyMssql@123;server=dfebcbcfaafeabdbcfacbdcbaeadbebabcdebdca-0;Database=carserviceDB;trusted_connection=false;Persist Security Info=False;Encrypt=False";
+    private string connectionString = "User ID=sa;password=examlyMssql@123;server=dffafdafebcfacbdcbaeadbebabcdebdca-0;Database=carserviceDB;trusted_connection=false;Persist Security Info=False;Encrypt=False";
 
-    public ActionResult Index()
+
+public ActionResult Index(string sortOrder)
     {
         List<Carservice> carservices = new List<Carservice>();
-try
-{
-        using (SqlConnection connection = new SqlConnection(connectionString))
+
+        try
         {
-            string query = "SELECT * FROM carservice";
-            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+                string query = "SELECT * FROM carservice";
+
+                if (!string.IsNullOrEmpty(sortOrder))
                 {
-                Carservice carservice = new Carservice();
-                carservice.id = Convert.ToInt32(reader["id"]);                   
-                carservice.car_name = reader["car_name"].ToString();
-                carservice.car_number = reader["car_number"].ToString();
-                carservice.car_varient = reader["car_varient"].ToString();
-                carservice.customer_name = reader["customer_name"].ToString();
-                carservice.complaint = reader["complaint"].ToString();
-                carservice.phonenumber = reader["phonenumber"].ToString();
-                carservice.address = reader["address"].ToString();
-                carservices.Add(carservice);
+                    query += " ORDER BY ";
+
+                    switch (sortOrder)
+                    {
+                        case "car_name":
+                            query += "car_name ASC";
+                            break;
+                        case "car_number":
+                            query += "car_number ASC";
+                            break;
+                        case "customer_name":
+                            query += "customer_name ASC";
+                            break;
+                        case "appointment_date":
+                            query += "appointment_date DESC"; // Assuming you have an appointment_date column
+                            break;
+                        default:
+                            query += "id ASC"; // Default sorting
+                            break;
+                    }
                 }
 
-                reader.Close();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Carservice carservice = new Carservice
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            car_name = reader["car_name"].ToString(),
+                            car_number = reader["car_number"].ToString(),
+                            car_varient = reader["car_varient"].ToString(),
+                            customer_name = reader["customer_name"].ToString(),
+                            complaint = reader["complaint"].ToString(),
+                            phonenumber = reader["phonenumber"].ToString(),
+                            address = reader["address"].ToString()
+                        };
+                        carservices.Add(carservice);
+                    }
+
+                    reader.Close();
+                }
             }
-        }return View(carservices);
-}
-catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-    return BadRequest("An error occurred while retrieving the carservices item.");
 
-}
-        // return View(carservices);
-
+            return View(carservices);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return BadRequest("An error occurred while retrieving the carservices item.");
+        }
     }
     public ActionResult Create()
     {
@@ -87,80 +115,7 @@ catch(Exception ex)
 }
         return RedirectToAction("Index");
     }
-    public ActionResult Edit(int id)
-    {
-        try{
-    Carservice carservice = null;
-    using (SqlConnection connection = new SqlConnection(connectionString))
-    {
-        string query = "SELECT * FROM Carservice WHERE id = @id";
-        using (SqlCommand command = new SqlCommand(query, connection))
-        {
-            command.Parameters.AddWithValue("@id", id);
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                carservice = new Carservice();
-
-                    carservice.id = Convert.ToInt32(reader["id"]);                   
-                carservice.car_name = reader["car_name"].ToString();
-                carservice.car_number = reader["car_number"].ToString();
-                carservice.car_varient = reader["car_varient"].ToString();
-                carservice.customer_name = reader["customer_name"].ToString();
-                carservice.complaint = reader["complaint"].ToString();
-                carservice.phonenumber = reader["phonenumber"].ToString();
-                carservice.address = reader["address"].ToString();                  
-            }
-
-            reader.Close();
-        }
-    }if (carservice == null)
-        {
-            return NotFound();
-        }
-    return View(carservice);
-        }catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        // You can handle the exception as per your requirements (e.g., logging, displaying an error message)
-        return BadRequest("An error occurred while retrieving the carservices item.");
-    }
-}
-    [HttpPost]
-    public ActionResult Edit(Carservice carservice )
-    {
-        try{
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            string query = "UPDATE Carservice SET car_name = @car_name, car_number = @car_number, car_varient = @car_varient, customer_name = @customer_name, complaint = @complaint, phonenumber = @phonenumber, address = @address WHERE id = @id";
-
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@id", carservice.id);
-                command.Parameters.AddWithValue("@car_name", carservice.car_name);
-                command.Parameters.AddWithValue("@car_number", carservice.car_number);
-                command.Parameters.AddWithValue("@car_varient", carservice.car_varient);
-                command.Parameters.AddWithValue("@customer_name", carservice.customer_name);
-                command.Parameters.AddWithValue("@complaint", carservice.complaint);
-                command.Parameters.AddWithValue("@phonenumber", carservice.phonenumber);
-                command.Parameters.AddWithValue("@address", carservice.address);
-                connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
-
-                if (rowsAffected == 0)
-                {
-                    // The provided ID does not exist in the Furniture table
-                    return NotFound();
-                }
-            }
-        }
-        } catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-        return RedirectToAction("Index");
-    }
+    
     public ActionResult Delete(int id)
     {
         try{
--- mvc1/dotnetapp/TestProject/UnitTest1.cs
+++ mvc1/dotnetapp/TestProject/UnitTest1.cs
@@ -2,6 +2,12 @@ using NUnit.Framework;
 using dotnetapp.Controllers;
 using dotnetapp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+using Moq;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace CarserviceControllerTests
 {
@@ -9,27 +15,39 @@ namespace CarserviceControllerTests
     public class CarserviceControllerTests
     {
         private CarserviceController _controller;
+        private Type controllerType;
+        private Type carserviceType;
+        private PropertyInfo[] properties;
 
         [SetUp]
         public void Setup()
         {
             // Initialize the controller before each test
             _controller = new CarserviceController();
+            carserviceType = typeof(dotnetapp.Models.Carservice);
+            properties = carserviceType.GetProperties();
+            controllerType = typeof(CarserviceController);
         }
+[Test]
+public void Index_ReturnsViewResultWithCarservicesSortedByName()
+{
+    // Arrange
+    var sortOrder = "car_name"; // Sort by car_name
 
-        [Test]
-        public void Index_ReturnsViewResultWithCarservices()
-        {
-            // Arrange
+    // Act
+    var result = _controller.Index(sortOrder) as ViewResult;
 
-            // Act
-            var result = _controller.Index() as ViewResult;
+    // Assert
+    Assert.IsNotNull(result);
+    Assert.IsInstanceOf<ViewResult>(result);
+    Assert.IsAssignableFrom(typeof(List<Carservice>), result.Model);
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
-            Assert.IsAssignableFrom(typeof(List<Carservice>), result.Model);
-        }
+    // Add more specific assertions based on your expected sorting behavior
+    // For example, you can check if the carservices in the result are sorted by name.
+    var model = result.Model as List<Carservice>;
+    // Add assertions here to check if the model is sorted by car_name.
+}
+
 
         [Test]
         public void Create_ReturnsViewResult()
@@ -43,20 +61,6 @@ namespace CarserviceControllerTests
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<ViewResult>(result);
         }
-        [Test]
-public void Edit_NonExistingId_ReturnsNotFoundResult()
-{
-    // Arrange
-    int nonExistingId = 100; // Assuming there is no Carservice item with ID 100
-
-    // Act
-    var result = _controller.Edit(nonExistingId) as NotFoundResult;
-
-    // Assert
-    Assert.IsNotNull(result);
-    Assert.IsInstanceOf<NotFoundResult>(result);
-}
-     
 
 [Test]
 public void Delete_NonExistingId_ReturnsNotFoundResult()
@@ -85,6 +89,122 @@ public void Delete_InvalidId_ReturnsBadRequestResult()
     Assert.IsNotNull(result);
     Assert.IsInstanceOf<BadRequestResult>(result);
 }
+
+[Test]
+public void TestIndexMethodExists()
+{
+    // Arrange
+    var controllerType = typeof(CarserviceController);
+    var methodName = "Index";
+
+    // Act
+    var indexMethod = controllerType.GetMethod(methodName);
+
+    // Assert
+    Assert.IsNotNull(indexMethod, $"{methodName} method should exist in CarserviceController.");
+}
+
+[Test]
+        public void TestCreateGetMethodExists()
+        {
+            // Arrange
+            MethodInfo createGetMethod = controllerType.GetMethod("Create", new Type[0]);
+
+            // Assert
+            Assert.IsNotNull(createGetMethod, "Create method should exist in CarserviceController.");
+        }
+
+        [Test]
+        public void TestCreatePostMethodExists()
+        {
+            // Arrange
+            MethodInfo createPostMethod = controllerType.GetMethod("Create", new Type[] { typeof(Carservice) });
+
+            // Assert
+            Assert.IsNotNull(createPostMethod, "Create POST method should exist in CarserviceController.");
+        }
+
+        [Test]
+        public void TestDeleteMethodExists()
+        {
+            // Arrange
+            MethodInfo createPostMethod = controllerType.GetMethod("Delete", new Type[] { typeof(int) });
+
+            // Assert
+            Assert.IsNotNull(createPostMethod, "Delete method should exist in CarserviceController.");
+        }
+
+        [Test]
+        public void TestCarserviceClassExists()
+        {
+            // Arrange
+            Type furnitureType = typeof(dotnetapp.Models.Carservice);
+
+            // Assert
+            Assert.IsNotNull(furnitureType, "Carservice class should exist.");            
+        }
+
+        [Test]
+        public void TestEmployeePropertiesExist()
+        {
+            // Assert
+            Assert.IsNotNull(properties, "Carservice class should have properties.");
+            Assert.IsTrue(properties.Length > 0, "Carservice class should have at least one property.");
+        }
+
+        [Test]
+        public void TestidProperty()
+        {
+            // Arrange
+            PropertyInfo idProperty = properties.FirstOrDefault(p => p.Name == "id");
+
+            // Assert
+            Assert.IsNotNull(idProperty, "id property should exist.");
+            Assert.AreEqual(typeof(int), idProperty.PropertyType, "id property should have data type 'int'.");
+        }
+
+        [Test]
+        public void Testcar_nameProperty()
+        {
+            // Arrange
+            PropertyInfo productProperty = properties.FirstOrDefault(p => p.Name == "car_name");
+
+            // Assert
+            Assert.IsNotNull(productProperty, "car_name property should exist.");
+            Assert.AreEqual(typeof(string), productProperty.PropertyType, "car_name property should have data type 'string'.");
+        }
+
+        [Test]
+        public void TestcarserviceDBContext_ClassExists_in_Models()
+        {
+            // Load the assembly at runtime
+            Assembly assembly = Assembly.Load("dotnetapp");
+            Type postType = assembly.GetType("dotnetapp.Models.carserviceDBContext");
+            Assert.NotNull(postType, "carserviceDBContext class does not exist.");
+        }
+
+        [Test]
+        public void Test_CreateViewFile_Exists()
+        {
+            string indexPath = Path.Combine(@"/home/coder/project/workspace/sriram-mvc-ado/mvc1/dotnetapp/Views/carservice", "Create.cshtml");
+            bool indexViewExists = File.Exists(indexPath);
+
+            Assert.IsTrue(indexViewExists, "Create.cshtml view file does not exist.");
+        }
+
+        [Test]
+        public void Test_IndexViewFile_Exists()
+        {
+            string indexPath = Path.Combine(@"/home/coder/project/workspace/sriram-mvc-ado/mvc1/dotnetapp/Views/carservice", "Index.cshtml");
+            bool indexViewExists = File.Exists(indexPath);
+
+            Assert.IsTrue(indexViewExists, "Index.cshtml view file does not exist.");
+        }
+
+        
+
+
+
 
     }
 }
